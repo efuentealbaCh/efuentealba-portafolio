aebda365597d96aa5f3fe6187352ef74da0fedfa
efuentealba <efuentealba@uct.cl>
2026-09-15 10:26:52 -0300

    feat: se mejora el SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://portafolio-efuentealba.netlify.app',
   vite: {
     plugins: [tailwindcss()]
   }
